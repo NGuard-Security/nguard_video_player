3563e35c489686313bd508e1ab3cbe91269574b5
Minsu Kim <kms0219kms@naver.com>
2024-03-31 16:49:28 +0000

    :egg:

--- assets/js/plyr/init.js
+++ assets/js/plyr/init.js
@@ -1,36 +1,27 @@
-let params = Object.fromEntries(new URLSearchParams(location.search).entries());
-
-let { src, poster } = params;
-
-let autoplay = params.autoplay || 0;
-let controls = params.controls || 1;
-let loop = params.loop || 0;
-
-let attr = "";
-let options = {};
-
-if (controls == 0) {
-  options = { controls: "" };
-} else {
-  attr += "controls ";
-}
-
-if (autoplay == 1) {
-  attr += "autoplay muted ";
-  options = { controls: "" };
-}
-
-if (loop == 1) {
-  attr += "loop";
-}
-
 document.querySelector(".container").innerHTML = `
-  <video crossorigin="anonymous" playsinline poster="${poster}" ${attr}>
-    <source src="${src}" type="video/mp4" size="1080">
-
-    <a href="${src}" download>Download</a>
+  <video crossorigin="anonymous" playsinline autoplay muted>
+    <source src="https://cdn.nguard.dev/assets/common/video/whistle.mp4" type="video/mp4" size="1080">
+    <a href="https://cdn.nguard.dev/assets/common/video/whistle.mp4" download>Download</a>
   </video>
 `;
 
-const player = new Plyr("video", options);
+let player = new Plyr("video", { controls: "" });
 window.player = player;
+
+// --------------------------
+
+function loadnormal() {
+	document.querySelector(".container").innerHTML = `
+	  <video crossorigin="anonymous" playsinline autoplay muted loop>
+	    <source src="https://cdn.nguard.dev/assets/common/video/background_video.mp4" type="video/mp4" size="1080">
+	    <a href="https://cdn.nguard.dev/assets/common/video/background_video.mp4" download>Download</a>
+	  </video>
+	`;
+
+	document.querySelector('video').removeEventListener('ended', loadnormal);
+
+	let player = new Plyr("video", { controls: "" });
+	window.player = player;
+}
+
+document.querySelector('video').addEventListener('ended', loadnormal);
